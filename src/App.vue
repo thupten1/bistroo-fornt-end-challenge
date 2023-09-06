7ec725adba7d3b4d15f28d77b7a18c1988450f24
Thupten <thupten09@hotmail.com>
2023-09-06 17:23:24 +0200

    front-end improvement and installed pinia

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
-<template>
+<template class="body">
   <div class="header">
-    <h1>welkom</h1>
   </div>
   <HelloWorld msg="Are you bored?"/>
   <div class="card">
@@ -14,6 +13,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 import { ref, onMounted } from 'vue'
 import getData from './api/Api-Request.vue'
+import './assets/css/globalstyle.css'
 
 const data = ref({ activity: '', type: '', participants:''})
 
@@ -24,37 +24,3 @@ onMounted(async () => {
   data.value.participants = result.participants;
 })
 </script>
-
-<style>
-.header{
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  color: #e50000;
-  background-color: antiquewhite;
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  justify-content: center;
-  z-index: -2;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-.card{
-  height: 30vh;
-  width: 20vw;
-  background-color: azure;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/css/globalstyle.css'
+import { createPinia } from 'pinia'
 
+apply.use(createPinia());
 createApp(App).mount('#app')
 
 
