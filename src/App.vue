c6b0ccdc6634a8d5946723986b379baa9f91e060
Thupten <thupten09@hotmail.com>
2023-09-06 10:36:52 +0200

    Axios API fetch is working

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header"><h1>welkom </h1></header>
+  <HelloWorld msg="Are you bored?"/>
+  <h2>{{ data.activity }}</h2>
+  <p>{{ data.type }}</p>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      data: []
+    }
+  },
+  mounted() {
+    axios
+      .get('https://www.boredapi.com/api/activity/')
+      .then((response) => {
+        this.data = response.data
+      })
   }
 }
 </script>
 
 <style>
+.header{
+  color: #e50000;
+  background-color: antiquewhite;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/api/Api-Request.vue
@@ -0,0 +1,13 @@
+<script setup>
+console.log("hi")
+    
+import axios from 'axios'
+
+    axios
+        .get('https://www.boredapi.com/api/activity/')
+        .then((response) => {
+            this.data = response.data
+      })
+    const data = Array(data)
+    console.log(data)
+</script>
