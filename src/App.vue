1897d22dd05dbf0ad0acf90aa018cd9faa382cae
Thupten <thupten09@hotmail.com>
2023-09-06 12:01:10 +0200

    managed to slipt the api request to diffrent file

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,42 @@
 <template>
-  <header class="header"><h1>welkom </h1></header>
+  <div class="header">
+    <h1>welkom</h1>
+  </div>
   <HelloWorld msg="Are you bored?"/>
-  <h2>{{ data.activity }}</h2>
-  <p>{{ data.type }}</p>
+  <div class="card">
+    <h2>{{ data.activity }}</h2>
+    <p>{{ data.type }}</p>
+    <p> Participants for this activity is {{ data.participants }}</p>
+  </div>
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import getData from './api/Api-Request.vue'
 
+const data = ref({ activity: '', type: '', participants:''})
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      data: []
-    }
-  },
-  mounted() {
-    axios
-      .get('https://www.boredapi.com/api/activity/')
-      .then((response) => {
-        this.data = response.data
-      })
-  }
-}
+onMounted(async () => {
+  const result = await getData();
+  data.value.activity = result.activity;
+  data.value.type = result.type;
+  data.value.participants = result.participants;
+})
 </script>
 
 <style>
 .header{
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
   color: #e50000;
   background-color: antiquewhite;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  justify-content: center;
+  z-index: -2;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -41,6 +44,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.card{
+  height: 30vh;
+  width: 20vw;
+  background-color: azure;
 }
 </style>
--- src/api/Api-Request.vue
+++ src/api/Api-Request.vue
@@ -1,13 +1,15 @@
-<script setup>
-console.log("hi")
-    
-import axios from 'axios'
-
-    axios
-        .get('https://www.boredapi.com/api/activity/')
-        .then((response) => {
-            this.data = response.data
-      })
-    const data = Array(data)
-    console.log(data)
+<script>
+export default async function getData() {
+  try {
+    const response = await fetch('https://www.boredapi.com/api/activity/');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
 </script>
